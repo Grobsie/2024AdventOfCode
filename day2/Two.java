1b21be36f5a681f41bc18d197644797e39169965
Grobsie <cruicer1@gmail.com>
2024-12-04 20:04:48 +0100

    day2 progress

--- day2/Two.java
+++ day2/Two.java
@@ -5,19 +5,37 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Two {
+    public boolean isIncreasing(int[] a){
+        boolean increase = false;
+
+        for (int position = 1; position < a.length; position++){  
+            if (a[position] > a[position-1]){
+                increase = true;
+            } else {
+                return false;
+            }
+        }
+        return increase;
+    }
     public static void main (String[] args) {
+        int safeReports = 0;
+        //read file
         try {
-            File input = new File("day1/Two_input_test.txt");
+            File input = new File("day2/Two_input_test.txt");
             Scanner myReader = new Scanner(input);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                String[] lineValues = data.split(" ");
-                
+                String[] lineValuesStr = data.split(" ");
+                int[] lineValues = new int[lineValuesStr.length];
+                for (int i = 0; i < lineValuesStr.length; i++){
+                    lineValues[i] = Integer.parseInt(lineValuesStr[i]);
+                }
+                //System.out.println(lineValues);
             }
             myReader.close();
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 }
