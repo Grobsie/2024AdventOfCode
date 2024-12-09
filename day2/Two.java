6da1e16132028890b532d5e813ba0daa1ecb1d41
Grobsie <cruicer1@gmail.com>
2024-12-09 20:12:41 +0100

    day 2, this was already hard for me

--- day2/Two.java
+++ day2/Two.java
@@ -5,23 +5,58 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Two {
-    public boolean isIncreasing(int[] a){
-        boolean increase = false;
-
+    public static boolean isIncreasing(int[] a){
+        int previousValue = a[0];
         for (int position = 1; position < a.length; position++){  
-            if (a[position] > a[position-1]){
-                increase = true;
+            if (a[position] > previousValue){
+                previousValue = a[position];
             } else {
                 return false;
             }
         }
-        return increase;
+        return true;
+    }
+
+    public static boolean isDecreasing(int[] a){
+        int previousValue = a[0];
+        for (int position = 1; position < a.length; position++){  
+            if (a[position] < previousValue){
+                previousValue = a[position];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCorrectRange(int[] a){
+        int previousValue = a[0];
+        for (int position = 1; position < a.length; position++){  
+            if (Math.abs(a[position] - previousValue) < 4){
+                previousValue = a[position];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] removeOneValue(int[] inputArray, int indexOfValue){
+        int[] result = new int[inputArray.length-1];
+        int count = 0;
+        for (int index = 0; index < inputArray.length; index++){
+            if (index != indexOfValue){
+                result[count] = inputArray[index];
+                count++;
+            }
+        }
+        return result;
     }
     public static void main (String[] args) {
         int safeReports = 0;
         //read file
         try {
-            File input = new File("day2/Two_input_test.txt");
+            File input = new File("day2/Two_input.txt");
             Scanner myReader = new Scanner(input);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
@@ -30,12 +65,36 @@ public class Two {
                 for (int i = 0; i < lineValuesStr.length; i++){
                     lineValues[i] = Integer.parseInt(lineValuesStr[i]);
                 }
-                //System.out.println(lineValues);
+                System.out.println(data);
+                //System.out.println("is this increasing: " + isIncreasing(lineValues));
+                //System.out.println("is this decreasing: " + isDecreasing(lineValues));
+                //System.out.println("is this in range  : " + isCorrectRange(lineValues));
+                int fixes = 0;
+                
+                if ((isIncreasing(lineValues) || isDecreasing(lineValues)) && isCorrectRange(lineValues)){
+                    //System.out.println("adding to safereports");
+                    System.out.println("this one is safe without changes");
+                    safeReports++;
+                } else if (fixes < 1){
+                    for (int x = 0; x < lineValues.length; x++){
+                        int[] adjustedData = removeOneValue(lineValues, x);
+                        if ((isIncreasing(adjustedData) || isDecreasing(adjustedData)) && isCorrectRange(adjustedData)){
+                            for (int i: adjustedData){
+                                System.out.print(i + " ");
+                            }
+                            System.out.println("This one is safe after one adjustment");
+                            safeReports++;
+                            fixes++;
+                            break;
+                        }
+                    }
+                }
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        System.out.println("Number of safe reports is: " + safeReports);
 
     }
 }
