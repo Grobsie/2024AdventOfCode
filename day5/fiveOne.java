0cd406a3466a3866f34bcc6c43707c056f83e550
Grobsie <cruicer1@gmail.com>
2024-12-15 21:12:39 +0100

    lookup in java arrays doesnt work:( Arraylist then

--- day5/fiveOne.java
+++ day5/fiveOne.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 
 public class fiveOne { 
     public static void main (String[] args) {
+        //
         int lineCount = 0;
         String[] dump = {"12","12"};
 
         try {
+            //read decode file and put this in the array valueParis
             File input = new File("day5/5_decode.txt");
             Scanner myReader = new Scanner(input);
             while (myReader.hasNextLine()) {
@@ -24,11 +26,38 @@ public class fiveOne {
                 String[] valuePairString = myReaderTwo.nextLine().split("\\|");
                 int firstValue = Integer.valueOf(valuePairString[0]);
                 int secondValue = Integer.valueOf(valuePairString[1]);
-                System.out.println(firstValue + " | " + secondValue);
+                //System.out.println(firstValue + " | " + secondValue);
                 valuePairs[lineCount] = new int[]{firstValue,secondValue};
                 lineCount++;
             }
             myReaderTwo.close();
+            //end of reading decode file
+
+            //read input strings
+            File dataInput = new File("day5/5_input.txt");
+            Scanner dataReader = new Scanner(dataInput);
+            while (dataReader.hasNextLine()){
+                String[] dataVStrings = dataReader.nextLine().split(",");
+                int[] dataValues = new int[dataVStrings.length];
+                for (int index = 0; index < dataVStrings.length;index++){
+                    dataValues[index] = Integer.valueOf(dataVStrings[index]);
+                }
+                for (int index = 0; index < dataValues.length;index++) {
+                    if (Arr)
+                }
+                //System.out.println(" ");
+            }
+            dataReader.close();
+            //finish reading input string
+            
+            //
+            //check if value is in match list
+                //value on the left or right side?
+                    //left
+                        //check if value after is present in array
+                            //yes? is it after the right value?
+                                //no? create array new array with equal size. 
+            //currently only do one line just to test
 
         } catch (FileNotFoundException e) {
             System.out.println(e);
