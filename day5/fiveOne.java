f97d0856251e6faebc93929d736ea430aceb5bc7
Grobsie <cruicer1@gmail.com>
2024-12-15 13:32:54 +0100

    c

--- day5/fiveOne.java
+++ day5/fiveOne.java
@@ -1,5 +1,19 @@
 package day5;
 
-public class fiveOne {
-    
+import java.io.File;
+import java.util.Scanner;
+
+public class fiveOne { 
+    public static void main (String[] args) {
+        try {
+            File input = new File("5_input.txt");
+            Scanner myReader = new Scanner(input);
+            while (myReader.hasNextLine()) {
+                
+            }
+            myReader.close(); 
+        } catch (Exception e){
+            System.out.println("oopsie, an error occured. Have a look: " + e);
+        }
+    }
 }
